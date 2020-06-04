6792f4e63080a63aa6090314aa857d5a60a76cb0
TheNickSkater <nicolasbeiglboeck@gmx.at>
2020-06-04 10:45:50 +0200

    Update sh_bodygrd_convars.lua

--- lua/autorun/sh_bodygrd_convars.lua
+++ lua/autorun/sh_bodygrd_convars.lua
@@ -8,8 +8,8 @@ hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_bodyguard_convars", function(t
 	tbl[ROLE_BODYGUARD] = tbl[ROLE_BODYGUARD] or {}
 
 	table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_kill_guard_teamkill", checkbox = true, desc = "ttt_bodygrd_kill_guard_teamkill (def. 1)"})
-  table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_win_alone", checkbox = true, desc = "ttt_bodygrd_win_alone (def. 0)"})
+	table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_win_alone", checkbox = true, desc = "ttt_bodygrd_win_alone (def. 0)"})
 	table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_damage_guarded_death", slider = true, min = 0, max = 50, decimal = 0, desc = "ttt_bodygrd_damage_guarded_death (def. 20)"})
-  table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_kill_guard_teamkill", slider = true, min = 0, max = 100, decimal = 2, desc = "ttt_bodygrd_kill_guard_teamkill (def. 1.5)"})
-  table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_damage_dealt_multiplier", slider = true, min = 0, max = 100, decimal = 2, desc = "ttt_bodygrd_damage_dealt_multiplier (def. 0.1)"})
+	table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_kill_guard_teamkill", slider = true, min = 0, max = 100, decimal = 2, desc = "ttt_bodygrd_kill_guard_teamkill (def. 1.5)"})
+	table.insert(tbl[ROLE_BODYGUARD], {cvar = "ttt_bodygrd_damage_dealt_multiplier", slider = true, min = 0, max = 100, decimal = 2, desc = "ttt_bodygrd_damage_dealt_multiplier (def. 0.1)"})
 end)
